1fc69565fb416a7f656cd9794973352e4b1f3610
Aleksandr Istomin <istomin.alexsandr@gmail.com>
2020-09-22 16:00:54 +0300

    22sep2020

---
+++ BuyJuiceApp/BuyJuiceApp/BuyJuiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyJuiceAppApp.swift
+//  BuyJuiceApp
+//
+//  Created by Александр Истомин on 21.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct BuyJuiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
