1f7d4420e71267e1a477ab9ec627921861031321
Aleksandr Istomin <istomin.alexsandr@gmail.com>
2020-09-29 16:28:45 +0300

    ShoeAPP

---
+++ NikeShoeApp/NikeShoeApp/NikeShoeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeShoeAppApp.swift
+//  NikeShoeApp
+//
+//  Created by Александр Истомин on 23.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct NikeShoeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
