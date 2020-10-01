7586c0b18d40e9b890b5a575a2445ec19243bf36
Aleksandr Istomin <istomin.alexsandr@gmail.com>
2020-10-01 16:06:20 +0300

    OneMore

---
+++ FurnitureApp/FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Александр Истомин on 30.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
